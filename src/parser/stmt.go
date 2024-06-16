add6f51ab26bd895ebf68a2e114b3452e2498359
Kishor Rathva <kishorrathva8298@gmail.com>
2024-06-16 22:47:04 +0530

    feat: implemented method to parse statements

--- src/parser/stmt.go
+++ src/parser/stmt.go
@@ -1,7 +1,21 @@
 package parser
 
-import "github.com/kishor82/lexer/src/ast"
+import (
+	"github.com/kishor82/lexer/src/ast"
+	"github.com/kishor82/lexer/src/lexer"
+)
 
 func parse_stmt(p *parser) ast.Stmt {
-	panic("Not implemented")
+	stmt_fn, exist := stmt_lu[p.currentTokenKind()]
+
+	if exist {
+		return stmt_fn(p)
+	}
+
+	expression := parse_expr(p, default_bp)
+	p.expect(lexer.SEMI_COLON)
+
+	return ast.ExpressionStmt{
+		Expression: expression,
+	}
 }
