8493718e3bd50ec6b5f7519e4628ce7cb779818d
Kishor Rathva <kishorrathva8298@gmail.com>
2024-06-16 22:45:19 +0530

    feat: added parser helper methods

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/kishor82/lexer/src/ast"
 	"github.com/kishor82/lexer/src/lexer"
 )
@@ -50,4 +52,22 @@ func (p *parser) advance() lexer.Token {
 
 func (p *parser) hasTokens() bool {
 	return p.pos < len(p.tokens) && p.currentTokenKind() != lexer.EOF
+}
+
+func (p *parser) expectError(expectedKind lexer.TokenKind, err any) lexer.Token {
+	token := p.currentToken()
+	kind := token.Kind
+
+	if kind != expectedKind {
+		if err == nil {
+			err = fmt.Sprintf("Expected %s but recieved %s instead.\n", lexer.TokenKindString(expectedKind), lexer.TokenKindString(kind))
+		}
+		panic(err)
+	}
+
+	return p.advance()
+}
+
+func (p *parser) expect(expectedKind lexer.TokenKind) lexer.Token {
+	return p.expectError(expectedKind, nil)
 }
